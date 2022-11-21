70c43e6535ed5f835831c918b897324111ed161a
Evgeny Busleiko <busleiko@mail.ru>
2022-11-21 21:55:47 +0300

    Make MVP

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import datetime
+import easygui
 
 
 
@@ -7,7 +8,7 @@ def make_answer (s,dig, error):
     current_date_time =str( datetime.datetime.now())
     if error==True: answer=current_date_time+' '+ s+' '+'решение не может быть найдено, ошибка в выражении '+'\n'
     else:
-        answer=current_date_time+' '+s+' = '+str (dig)+'\n'
+        answer=current_date_time+' '+s+' = '+dig+'\n'
     return answer
 
 def save_log(answer_in_string):
@@ -15,8 +16,7 @@ def save_log(answer_in_string):
 
         fh.write(answer_in_string)
 
-        print('Я успешно загрузил в файл следующую информацию')
-        print(answer_in_string)
+        easygui.msgbox(answer_in_string,'Я успешно загрузил в файл следующую информацию')
 # text='2+2'
 # digital=(2+3j)
 # answer_in_string=make_answer(text,digital,True)
